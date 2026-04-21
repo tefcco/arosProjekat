45c15cf8626bf51ca22795cbba32be93ed01517c
tefcco <administrator@DESKTOP-I0292P0.localdomain>
2026-04-21 22:27:15 +0200

    Kreiranje pipe-ova

--- projekat.c
+++ projekat.c
@@ -3,10 +3,26 @@
 #include <string.h>
 #include <unistd.h>
 #include <semaphore.h>
+#include <sys/mman.h>
+#include <sys/wait.h>
 
 
 
 int main(){
+
+int pipe1[2], pipe2[2];
+
+pipe(pipe1);
+if(pipe(pipe1)==-1){
+	perror("Pipe1 ne radi.");
+	exit(1);
+}
+
+pipe(pipe2);
+if(pipe(pipe2)==-1){
+	perror("Pipe 2 ne radi.");
+	exit(1);
+}
 
 return 0;
 }
